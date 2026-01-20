ee8c382f4c449a93e1ea8b9efa87fc9fedc855c5
Louis <louis.dittmar@gmx.de>
2026-01-20 11:41:27 +0100

    fix: Add role to registration test

--- tests/Feature/Auth/RegistrationTest.php
+++ tests/Feature/Auth/RegistrationTest.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Enums\UserRole;
+
 test('registration screen can be rendered', function () {
     $response = $this->get(route('register'));
 
@@ -12,6 +14,7 @@ test('new users can register', function () {
         'email' => 'test@example.com',
         'password' => 'password',
         'password_confirmation' => 'password',
+        'role' => UserRole::USER->value,
     ]);
 
     $this->assertAuthenticated();
