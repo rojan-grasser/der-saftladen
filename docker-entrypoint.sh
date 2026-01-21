6975c67e2e318a4ee7dae616ddc80c838ac3803d
Rojan Grasser <rojan.jamie@gmail.com>
2026-01-21 11:02:10 +0100

    fix(#43): Added runmodes via env vars in docker entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 set -e
 
-# Run migrations
-php artisan migrate --force
+if [ "$RUNMODE" -eq "server" ]; then
+    # Run migrations
+    php artisan migrate --force
 
-# Start Octane
-php artisan octane:start --host=0.0.0.0 --port=9000 --server=roadrunner
+    # Start Octane
+    php artisan octane:start --host=0.0.0.0 --port=9000 --server=roadrunner
+elif [ "$RUNMODE" -eq "housekeeping" ]; then
+    php artisan app:housekeeping
+else
+    echo "[ERROR]: Unknown runmode $RUNMODE"
+fi
