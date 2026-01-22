93dbf5a490665f862529c14cc1d2c0393cdb8e8d
Louis <louis.dittmar@gmx.de>
2026-01-22 13:51:35 +0100

    feat: Add Appointment model

---
+++ app/Models/Appointment.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Appointment extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'location',
+        'start_time',
+        'end_time',
+        'user_id',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'start_time' => 'datetime',
+            'end_time' => 'datetime',
+        ];
+    }
+
+    public function creator(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+}
