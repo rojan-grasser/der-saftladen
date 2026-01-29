c26a0ca95ab0b2058f9739164472b42cc4430d5d
Rojan Grasser <rojan.jamie@gmail.com>
2026-01-29 23:28:30 +0100

    feat: added new user which is initially being created if the deployment is a feature deployment

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,5 +4,10 @@ set -e
 # Run migrations
 php artisan migrate --force
 
+# Run feature deployment initialization if FEATURE env var is set
+if [ -n "$FEATURE" ]; then
+  php artisan app:feature-deployment-init
+fi
+
 # Start Octane
 php artisan octane:start --host=0.0.0.0 --port=9000 --server=roadrunner
