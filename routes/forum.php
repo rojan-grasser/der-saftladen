8806705a325dd3a97b719ae0631337143598e3ae
Rojan Grasser <rojan.jamie@gmail.com>
2026-03-13 11:52:28 +0100

    fix: Cleaned up routes

---
+++ routes/forum.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Http\Controllers\Forum\PostController;
+use App\Http\Controllers\Forum\PostReactionController;
+use App\Http\Controllers\Forum\ProfessionController as ForumProfessionController;
+use App\Http\Controllers\Forum\TopicController;
+use Illuminate\Support\Facades\Route;
+
+Route::middleware(['auth', 'verified', 'active'])
+    ->prefix('forum')
+    ->group(function () {
+        Route::get('professions', [ForumProfessionController::class, 'index'])->name('forum.professions');
+
+        // Route::resource('profession', TopicController::class);
+        Route::get('profession/{professionId}', [TopicController::class, 'index'])->name('topics.index');
+        Route::post('profession/{professionId}/topics', [TopicController::class, 'store'])->name('topics.store');
+
+        Route::middleware([])
+            ->prefix('profession/{professionId}')
+            ->group(function () {
+                Route::get('/topics/{topicId}', [TopicController::class, 'show'])->name('topics.show');
+                Route::put('/topics/{topicId}', [TopicController::class, 'update'])->name('topics.update');
+
+                Route::middleware([])
+                    ->prefix('topics/{topicId}')
+                    ->group(function () {
+                        Route::get('posts/{postId}/reactions', [PostReactionController::class, 'index'])->name('posts.reactions.index');
+                        Route::post('posts/{postId}/reactions', [PostReactionController::class, 'store'])->name('posts.reactions.store');
+                        Route::delete('posts/{postId}/reactions', [PostReactionController::class, 'destroy'])->name('posts.reactions.destroy');
+
+                        Route::resource('posts', PostController::class);
+                    });
+            });
+    });
