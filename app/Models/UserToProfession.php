956f08151d59fd5a13b24007c7e0d3b971887db4
Rojan Grasser <rojan.jamie@gmail.com>
2026-03-16 11:07:35 +0100

    fix: cleaned up admin controllers a little bit

--- app/Models/UserToProfession.php
+++ app/Models/UserToProfession.php
@@ -3,8 +3,21 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class UserToProfession extends Model
 {
-    //
+    protected $table = 'user_to_profession';
+
+    protected $fillable = ['user_id', 'profession_id'];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function profession(): BelongsTo
+    {
+        return $this->belongsTo(Profession::class);
+    }
 }
