8b92b7b5acfe29fd56edb8b188e6ae483424668d
Louis <louis.dittmar@gmx.de>
2026-03-16 11:16:51 +0000

    fix: Deletet Everything hat has something todo for

--- config/webpush.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'vapid' => [
-        'subject' => env('VAPID_SUBJECT', 'mailto:admin@example.com'),
-        'public_key' => env('VAPID_PUBLIC_KEY'),
-        'private_key' => env('VAPID_PRIVATE_KEY'),
-    ],
-];
