69814b8c245aec164ef950c61f878473cf8b490e
Louis <louis.dittmar@gmx.de>
2026-03-16 23:42:26 +0100

    Addete More termins to Seedign

--- database/seeders/AppointmentSeeder.php
+++ database/seeders/AppointmentSeeder.php
@@ -24,6 +24,17 @@ class AppointmentSeeder extends Seeder
         $today = Carbon::today();
 
         $appointments = [
+            // Vergangene Termine
+            ['title' => 'Jahresplanung 2025', 'location' => 'Konferenzraum', 'description' => 'Ziele und Meilensteine fuer das Jahr 2025 festlegen.', 'day_offset' => -84, 'hour' => 9, 'minute' => 0, 'duration' => 120, 'color' => AppointmentColor::BLUEBERRY],
+            ['title' => 'Lieferantenbesuch Bioland', 'location' => 'Lager', 'description' => 'Neue Produkte und Konditionen fuer die Frischesaison besprechen.', 'day_offset' => -60, 'hour' => 10, 'minute' => 0, 'duration' => 90, 'color' => AppointmentColor::BASIL],
+            ['title' => 'Inventur Februar', 'location' => 'Filiale', 'description' => 'Vollstaendige Bestandsaufnahme aller Lager- und Verkaufsware.', 'day_offset' => -45, 'hour' => 7, 'minute' => 0, 'duration' => 180, 'color' => AppointmentColor::GRAPHITE],
+            ['title' => 'Teammeeting Q1', 'location' => 'Meetingraum', 'description' => 'Quartalsrueckblick und Ausblick auf Q2.', 'day_offset' => -30, 'hour' => 14, 'minute' => 0, 'duration' => 60, 'color' => AppointmentColor::PEACOCK],
+            ['title' => 'Schulung Kassensystem', 'location' => 'Buero', 'description' => 'Neue Funktionen des Kassensystems kennenlernen.', 'day_offset' => -20, 'hour' => 11, 'minute' => 0, 'duration' => 90, 'color' => AppointmentColor::TANGERINE],
+            ['title' => 'Kundenevent Fruehlingsmarkt', 'location' => 'Verkaufsraum', 'description' => 'Verkostung und Praesentationen neuer Fruehlingsprodukte.', 'day_offset' => -14, 'hour' => 10, 'minute' => 0, 'duration' => 240, 'color' => AppointmentColor::FLAMINGO],
+            ['title' => 'Social-Media-Rueckblick', 'location' => 'Marketing Ecke', 'description' => 'Performance der letzten Kampagnen auswerten.', 'day_offset' => -7, 'hour' => 13, 'minute' => 0, 'duration' => 60, 'color' => AppointmentColor::SAGE],
+            ['title' => 'Wochenabschluss', 'location' => 'Buero', 'description' => 'Offene Punkte der Woche abschliessen und naechste Woche vorbereiten.', 'day_offset' => -2, 'hour' => 16, 'minute' => 0, 'duration' => 45, 'color' => AppointmentColor::LAVENDER],
+
+            // Aktuelle Woche
             ['title' => 'Wochenstart und Prioritaeten', 'location' => 'Buero', 'description' => 'Offene Aufgaben sortieren und Prioritaeten fuer die Woche festlegen.', 'day_offset' => 0, 'hour' => 9, 'minute' => 0, 'duration' => 60, 'color' => AppointmentColor::PEACOCK],
             ['title' => 'Lieferantenabstimmung', 'location' => 'Telefonat', 'description' => 'Verfuegbarkeit und Lieferfenster fuer kommende Bestellungen abstimmen.', 'day_offset' => 3, 'hour' => 11, 'minute' => 30, 'duration' => 45, 'color' => AppointmentColor::SAGE],
             ['title' => 'Team-Retrospektive', 'location' => 'Meetingraum', 'description' => 'Rueckblick auf die laufenden Prozesse und offene Verbesserungen.', 'day_offset' => 6, 'hour' => 15, 'minute' => 0, 'duration' => 75, 'color' => AppointmentColor::GRAPE],
@@ -41,6 +52,16 @@ class AppointmentSeeder extends Seeder
             ['title' => 'Quartalsausblick', 'location' => 'Konferenzraum', 'description' => 'Entwicklung der naechsten drei Monate mit Fokus auf Umsatz und Aktionen planen.', 'day_offset' => 84, 'hour' => 9, 'minute' => 0, 'duration' => 120, 'color' => AppointmentColor::FLAMINGO],
         ];
 
+        // Mehrtaegige Termine (eigene end_time)
+        $multiDayAppointments = [
+            ['title' => 'Branchenmesse BioNord', 'location' => 'Messezentrum Hamburg', 'description' => 'Zwei Tage auf der Fachmesse fuer Bioprodukte und Naturkost.', 'day_offset' => -50, 'hour' => 9, 'minute' => 0, 'end_offset' => -49, 'end_hour' => 18, 'color' => AppointmentColor::BLUEBERRY],
+            ['title' => 'Betriebsausflug Team', 'location' => 'Schwarzwald', 'description' => 'Dreitaegiger Teamausflug zur Staerkung des Zusammenhalts.', 'day_offset' => -21, 'hour' => 8, 'minute' => 0, 'end_offset' => -19, 'end_hour' => 17, 'color' => AppointmentColor::FLAMINGO],
+            ['title' => 'Umbau Verkaufsflaeche', 'location' => 'Filiale', 'description' => 'Neugestaltung der Verkaufsflaeche ueber zwei Tage.', 'day_offset' => 5, 'hour' => 7, 'minute' => 0, 'end_offset' => 6, 'end_hour' => 20, 'color' => AppointmentColor::TANGERINE],
+            ['title' => 'Foodfestival Teilnahme', 'location' => 'Stadtpark', 'description' => 'Drei Tage als Aussteller beim regionalen Foodfestival.', 'day_offset' => 20, 'hour' => 10, 'minute' => 0, 'end_offset' => 22, 'end_hour' => 21, 'color' => AppointmentColor::TOMATO],
+            ['title' => 'Schulung neue Mitarbeiter', 'location' => 'Seminarraum', 'description' => 'Zweitaegige Einarbeitung und Schulung neuer Teammitglieder.', 'day_offset' => 40, 'hour' => 9, 'minute' => 0, 'end_offset' => 41, 'end_hour' => 17, 'color' => AppointmentColor::LAVENDER],
+            ['title' => 'Strategieworkshop Herbst', 'location' => 'Tagungshotel', 'description' => 'Dreitaegiger Workshop zur Herbst- und Winterstrategie.', 'day_offset' => 70, 'hour' => 9, 'minute' => 0, 'end_offset' => 72, 'end_hour' => 16, 'color' => AppointmentColor::GRAPE],
+        ];
+
         foreach ($appointments as $plan) {
             $start = $today->copy()->addDays($plan['day_offset'])->setTime($plan['hour'], $plan['minute']);
 
@@ -55,6 +76,25 @@ class AppointmentSeeder extends Seeder
                     'color' => $plan['color']->value,
                     'start_time' => $start,
                     'end_time' => $start->copy()->addMinutes($plan['duration']),
+                ],
+            );
+        }
+
+        foreach ($multiDayAppointments as $plan) {
+            $start = $today->copy()->addDays($plan['day_offset'])->setTime($plan['hour'], $plan['minute']);
+            $end = $today->copy()->addDays($plan['end_offset'])->setTime($plan['end_hour'], 0);
+
+            Appointment::updateOrCreate(
+                [
+                    'user_id' => $owner->id,
+                    'title' => $plan['title'],
+                ],
+                [
+                    'description' => $plan['description'],
+                    'location' => $plan['location'],
+                    'color' => $plan['color']->value,
+                    'start_time' => $start,
+                    'end_time' => $end,
                 ],
             );
         }
