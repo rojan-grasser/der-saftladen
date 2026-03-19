d2c56bd42903c86ad627d71cff8fac7238609d45
Rojan Grasser <rojan.jamie@gmail.com>
2026-03-19 11:59:56 +0100

    fix: Fixed handle inertia request thingy

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -35,15 +35,13 @@ class HandleInertiaRequests extends Middleware
      */
     public function share(Request $request): array
     {
-        $user = $request->user();
-
         return [
             ...parent::share($request),
             'name' => config('app.name'),
             'auth' => [
-                'user' => $user ? [
-                    ...$user->toArray(),
-                    'roles' => $user->rolePayload(),
+                'user' => $request->user() ? [
+                    ...$request->user()->toArray(),
+                    'roles' => $request->user()->roles,
                 ] : null,
             ],
             'sidebarOpen' => ! $request->hasCookie('sidebar_state') || $request->cookie('sidebar_state') === 'true',
